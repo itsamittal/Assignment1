fad80a587e2f6283d5e2959f08385c944ea6a8d2
Abhishek <51646455+itsamittal@users.noreply.github.com>
2025-07-12 23:49:44 +0530

    Update Tutedude.py

--- Tutedude.py
+++ Tutedude.py
@@ -1,55 +1,38 @@
-
-# coding: utf-8
-
-# # Task 1: Perform Basic Mathematical Operations
-
-# In[ ]:
-
+# Task 1: Perform Basic Mathematical Operations
 
 # 1.  Takes two numbers as input from the user.
 
-
-# In[1]:
-
-
+# Addition of two number.
 Number_1 = int(input('Enter the first number :'))
 Number_2 = int(input('Enter the second number :')) 
 addition = Number_1 + Number_2
 print(addition)
 
 
-# In[6]:
-
-
+# Substraction of two number.
 Number_1 = int(input('Enter the first number :'))
 Number_2 = int(input('Enter the second number :')) 
 subtraction = Number_1 - Number_2
 print(subtraction)
 
 
-# In[9]:
-
-
+# Multiplication of two number.
 Number_1 = int(input('Enter the first number :'))
 Number_2 = int(input('Enter the second number :')) 
 multiplication= Number_1 * Number_2
 print(multiplication)
 
 
-# In[12]:
-
-
+# Division of two nu mber.
 Number_1 = float(input('Enter the first number :'))
 Number_2 = float(input('Enter the second number :')) 
 division = Number_1 / Number_2
 print(division)
 
 
-# # Task 2: Create a Personalized Greeting
 
-# In[ ]:
-
-
+# Task 2: Create a Personalized Greeting
+# a personalized greeting message using the full name.
 First_name = str(input('Enter your first name:'))
 Last_name = str(input('Enter last name:'))
 Full_name = ( First_name + " " +Last_name )
